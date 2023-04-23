2b4dccf7f749d82cb857dc1e38f0b89edcda22db
Miholics Bence <bence.miholics@evosoft.com>
2023-04-23 23:38:36 +0200

    destr.+ copycon

--- Circle.cpp
+++ Circle.cpp
@@ -3,32 +3,33 @@
 #include "Point.h"
 #include "Circle.h"
 
-Circle::Circle(double x, double y, double radius)
+Circle::Circle(double x, double y, double radius) : radius(radius)
 {
-
+	mid = new Point(x, y);
 }
 
-Circle::Circle(Circle& c)
+Circle::Circle(Circle& c) : radius(c.radius)
 {
-
+	mid = new Point(*(c.mid));//or getters
 }
 
 double Circle::get_area()
 {
+	return 3.14 * radius * radius;
 
 }
 
 Point* Circle::get_point()
 {
-
+	return mid;
 }
 
 std::string Circle::to_string()
 {
-
+	return mid->to_string() + "r=" + std::to_string(radius);
 }
 
 Circle::~Circle()
 {
-
+	delete mid;
 }
